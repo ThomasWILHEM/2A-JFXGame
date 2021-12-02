f8e0a7deea0c7c85eacc8af91633794253989994
benjamin <benjamin.labonne@etu.uca.fr>
2021-12-02 11:57:13 +0100

    Ajout des classes de bases,TODO : rem anchorpane

---
+++ Jeu/src/modele/ChampVision.java
@@ -0,0 +1,10 @@
+package modele;
+
+class champVision {
+    private int distance;
+    private int angle;
+    public champVision(int dist,int angle) {
+        this.distance=dist;
+        this.angle=angle;
+    }
+}
