5985ef29fabf2202e623e007d94903e68087940b
Benjamin L <Benjamin.Labonne.03@outlook.fr>
2022-01-14 14:14:07 -0500

    AJout du break qui fait tourner les gardes lol

--- Jeu/src/modele/DeplaceurGarde.java
+++ Jeu/src/modele/DeplaceurGarde.java
@@ -11,10 +11,10 @@ public class DeplaceurGarde extends Deplaceur {
     public int deplacer(Carte c, Entity e, Position pVoulue) {
         System.out.println(((Personnage) e).getDirection());
         switch(((Personnage) e).getDirection()) {
-            case 'U' : ((Personnage) e).setDirection('R');
-            case 'R' : ((Personnage) e).setDirection('D');
-            case 'D' : ((Personnage) e).setDirection('L');
-            case 'L' : ((Personnage) e).setDirection('U');
+            case 'U' : ((Personnage) e).setDirection('R');break;
+            case 'R' : ((Personnage) e).setDirection('D');break;
+            case 'D' : ((Personnage) e).setDirection('L');break;
+            case 'L' : ((Personnage) e).setDirection('U');break;
         }
         return 0;
     }
